28dc727246b3256810a8e3720db315ae87ad1777
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2026-04-25 21:27:39 +0530

    encapsulation in oops

--- oops.java
+++ oops.java
@@ -1,17 +1,24 @@
  class Calculator{
     
-     int a ;
-     int b ;
-     int sum(){
-        return a+b ;
+      int count ;
+     private int amount = 5;
+     int total(){
+        return count*amount ;
      }
+     int getamount(){
+      return amount ;
+     }
+   void setamount(int x){
+       amount = x ;
+    }
+
 }
 public class oops{
     public static void main (String args[]){
       Calculator c =  new Calculator() ;
-       c.a = 5 ;
-       c.b = 6 ;
-       System.out.println(c.sum());
+      c.count = 2 ;
+      c.setamount(10);
+      System.out.println(c.total());
 
     }
 }
---
+++ strings.java
@@ -0,0 +1,14 @@
+
+import com.sun.source.tree.PackageTree;
+
+public class strings{
+    public static void main(String args[]){
+        String s =  "sumit ka";
+        StringBuffer sb = new StringBuffer("sumuit kashhyap");
+        sb.delete(2, 5);
+        sb.append(" rajput");
+        sb.insert(2, "mi");
+        sb.deleteCharAt(9);
+        System.out.print(sb);
+    }
+}
