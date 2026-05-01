fbdb5576e68cf434a4d1e2927531ba77d2beb9af
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2026-05-01 22:57:56 +0530

    Polymorphism And Multiple Inheritance

--- tools/Polimorphism.java
+++ tools/Polimorphism.java
@@ -15,7 +15,8 @@
         System.err.println("I an C");
     }
  }
- class D {
+//  Multilevel inheritance
+ class D extends C{
  void show(){
         System.err.println("I an D");
     }
@@ -23,6 +24,7 @@
 public class Polimorphism{
 //    if a child class holds the property of parent class then We can initilize the parent class object and then point it to the subclasse 
     public static void main(String[] args) {
+      
          A obj = new A();
          obj.show();
           obj = new B();
@@ -33,5 +35,9 @@ public class Polimorphism{
         //  obj.show();
         //  not working for Class D because D not inherit the properties of A 
         // or we can saw that D is not the child class of A ; 
+
+        //   B obj2 = new B();
+        // obj2 = new A();
+        // not possible if we create obj of sub class and initial new obj to that obj with superclass ;
     }
 }
