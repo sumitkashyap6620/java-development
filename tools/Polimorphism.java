66abd5dcb31fda5d6f9602ea23806a1311ecf724
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2026-05-01 22:52:03 +0530

    Polymorphism in java

---
+++ tools/Polimorphism.java
@@ -0,0 +1,37 @@
+
+  class A{
+      
+    void show(){
+        System.err.println("I an A");
+    }
+ }
+ class B extends A{
+ void show(){
+        System.err.println("I an B");
+    }
+ }
+ class C extends A {
+ void show(){
+        System.err.println("I an C");
+    }
+ }
+ class D {
+ void show(){
+        System.err.println("I an D");
+    }
+ }
+public class Polimorphism{
+//    if a child class holds the property of parent class then We can initilize the parent class object and then point it to the subclasse 
+    public static void main(String[] args) {
+         A obj = new A();
+         obj.show();
+          obj = new B();
+         obj.show();
+          obj = new C();
+         obj.show();
+        //  obj = new D();
+        //  obj.show();
+        //  not working for Class D because D not inherit the properties of A 
+        // or we can saw that D is not the child class of A ; 
+    }
+}
