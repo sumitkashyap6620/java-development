3e57692ab83d4fa8eec13aee415c5ba9ff0cd134
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2026-05-02 11:19:29 +0530

    Final keyword in java

---
+++ tools/UseFinal.java
@@ -0,0 +1,22 @@
+//  Final -> variable ,method,class !
+ final class A{
+    
+}
+// class  B extends  A{
+    // Class A is Final it can not be inherit in another class
+// }
+class B {
+  final  void show(){
+
+    }
+}
+
+class C extends B {
+    //     void show(){
+//  not possible because in B already defined show method whicjh is final 
+    // }
+}
+public class UseFinal{
+   final int x  = 5 ;
+//    x = 3 ;   x is final can not bhe change 
+}
