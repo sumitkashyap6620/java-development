1f80bbb019557fae5d1abdd4543ca392bd8a9e35
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2026-05-03 15:33:41 +0530

    by using Interface

--- OopsFlexibility.java
+++ OopsFlexibility.java
@@ -1,17 +1,50 @@
-//  By using abstract class 
+                                       //  By using abstract class 
 //               |
 //               |
-abstract class Computer{
+// abstract class Computer{
 
-    abstract void dev();
+//     abstract void dev();
+// }
+// class Laptop extends Computer{
+//     void dev(){
+//         System.out.println("Coding by Laptop !");
+//     }
+// }
+// class Dextop extends Computer{
+//     void dev(){
+//         System.out.println("Coding by Dextop !");
+//     }
+// }
+// class Developer{
+//     void dev(Computer lap){
+//         lap.dev();
+//     }
+// }
+// public class OopsFlexibility{
+// public static void main(String[] args) {
+//     Developer sumit = new Developer() ;
+//     Computer dell =  new Laptop();
+//     Computer hp =  new Dextop();
+//     sumit.dev(dell);
+//     sumit.dev(hp);
+// }
+// }
+
+                                     //  By using abstract class 
+//               |
+//               |
+
+interface Computer{
+
+    public void dev();
 }
-class Laptop extends Computer{
-    void dev(){
+class Laptop implements Computer{
+   public void dev(){
         System.out.println("Coding by Laptop !");
     }
 }
-class Dextop extends Computer{
-    void dev(){
+class Dextop implements Computer{
+    public void dev(){
         System.out.println("Coding by Dextop !");
     }
 }
